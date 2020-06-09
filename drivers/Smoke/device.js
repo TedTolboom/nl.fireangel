5be8dcc56887608fc5d7f33becc8739f45e72a8d
Momotica <menno@momotica.nl>
2020-06-09 16:21:09 +0200

    Smoke test

--- drivers/Smoke/device.js
+++ drivers/Smoke/device.js
@@ -4,14 +4,45 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class FireAngelSMOKE extends ZwaveDevice {
 
-    async onMeshInit() {
+    onMeshInit() {
 
     	this.enableDebug();
         // print the node's info to the console
         this.printNode();
 
         this.registerCapability('measure_battery', 'BATTERY');
-        this.registerCapability('alarm_smoke', 'NOTIFICATION');
+
+        this.registerCapability('alarm_smoke', 'NOTIFICATION', {
+			getOpts: {
+				getOnStart: true, // get the initial value on app start
+				//pollInterval: 'poll_interval' // maps to device settings
+			},
+			report: 'NOTIFICATION_REPORT',
+			reportParser: report => {
+				if (report && report.hasOwnProperty('Notification Type') && report.hasOwnProperty('Event (Parsed)')) {
+					if (report['Event (Parsed)'] === 'Smoke detected, Unknown Location' && report['Notification Type'] === 'Smoke') return true; 
+					if (report['Event (Parsed)'] === 'Event inactive' && report['Notification Type'] === 'Smoke') return false;
+				
+				return null;
+				}	
+			},
+		});
+
+        this.registerCapability('smoke_test', 'NOTIFICATION', {
+			getOpts: {
+				getOnStart: true, // get the initial value on app start
+				//pollInterval: 'poll_interval' // maps to device settings
+			},
+			report: 'NOTIFICATION_REPORT',
+			reportParser: report => {
+				if (report && report.hasOwnProperty('Notification Type') && report.hasOwnProperty('Event (Parsed)')) {
+					if (report['Event (Parsed)'] === 'Smoke Alarm Test' && report['Notification Type'] === 'Smoke') return true; 
+					if (report['Event (Parsed)'] === 'Event inactive' && report['Notification Type'] === 'Smoke') return false;
+				
+				return null;
+				}	
+			},
+		});
     }
 }
 
